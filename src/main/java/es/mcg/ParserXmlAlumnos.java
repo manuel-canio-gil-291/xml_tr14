c0ae35b0f9498b54ab6dc73348658cc4d7026118
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-12-05 10:03:52 +0100

    Parser XML Alumnos

--- src/main/java/es/mcg/ParserXmlAlumnos.java
+++ src/main/java/es/mcg/ParserXmlAlumnos.java
@@ -1,8 +1,11 @@
 package es.mcg;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -60,10 +63,17 @@ public class ParserXmlAlumnos {
     public static void main(String[] args) {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         File file = null;
+        FileWriter fileWriter = null;
+        PrintWriter printWriter = null;
         List<Alumno> alumnosinUnidades = null;
+        Alumno alumno = null;
+        Double calificacionM = 0.0, calificacionP = 0.0, media = 0.0;
+        String nombreA = "", nombreB = "";
         try 
         {
             file = new File("salida-alumnos.txt");
+            fileWriter = new FileWriter(file);
+            printWriter = new PrintWriter(fileWriter);
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(new File("alumnos.xml"));
             Element rootElement = document.getDocumentElement();
@@ -76,11 +86,37 @@ public class ParserXmlAlumnos {
                 String nombre = nodeAlumno.getAttributes().getNamedItem("nombre").getTextContent();
                 Integer edad = Integer.valueOf(nodeAlumno.getAttributes().getNamedItem("edad").getTextContent());
                 Double calificacion = Double.valueOf(nodeAlumno.getElementsByTagName("calificacion").item(0).getTextContent());
+                media += calificacion;
+                if(calificacionM == 0.0 && calificacionP == 0.0)
+                {
+                    calificacionM = calificacion;
+                    nombreA = nombre;
+                    calificacionP = calificacion;
+                    nombreB = nombre;
+                }
+                else if(calificacion > calificacionM)
+                {
+                    calificacionM = calificacion;
+                    nombreA = nombre;
+                }
+                if(calificacion < calificacionP)
+                {
+                    calificacionP = calificacion;
+                    nombreB = nombre;
+                }
                 Boolean unidadesPendientes = Boolean.valueOf(nodeAlumno.getElementsByTagName("unidadesPendientes").item(0).getTextContent());
-                
+                alumno = new Alumno(nombre, edad, calificacion, unidadesPendientes);
+                if(unidadesPendientes == false)
+                {
+                    alumnosinUnidades.add(alumno);
+                }
             }
-            
-
+            printWriter.println("Listado de alumnos sin unidades pendientes:");
+            printWriter.println(alumnosinUnidades);
+            printWriter.println("Alumno con mejor calificacion: "+nombreA);
+            printWriter.println("Alumno con peor calificacion: "+nombreB);
+            printWriter.println("Nota media de clase: "+(media/nodeListAlumno.getLength()));
+            printWriter.flush();
         } 
         catch (ParserConfigurationException | SAXException | IOException xmlException) 
         {
